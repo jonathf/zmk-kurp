29cd4f97c54739f2319aab10cc5a1e6d23a761c5
Jonathan Feinberg <jonathf@gmail.com>
2023-10-01 15:18:57 +0200

    combos fix

--- config/combos.h
+++ config/combos.h
@@ -1,18 +1,18 @@
 /*
-      ╭────┬────┬────┬────╮  ╭────┬────┬────┬────╮
- 0    1    2    3    4    5  6    7    8    9   10   11
-                                   INS  DEL
-12   13   14   15   16   17 18   19   20   21   22   23
-       LALT ESC  TAB              ENTER BKSP RALT
-24   25   26   27   28   29 30   31   32   33   34   35
-      ╰────┴────┴────┴────╯  ╰────┴────┴────┴────╯
-               36   37   38 39   40   41
+  ╭────┬────┬────┬────╮  ╭────┬────┬────┬────╮
+  0    1    2    3    4  5    6    7    8    9
+                               INS  DEL
+ 10   11   12   13   14 15   16   17   18   19
+        ESC  TAB              ENTER BKSP
+ 20   21   22   23   24 25   26   27   28   29
+  ╰────┴────┴────┴────╯  ╰────┴────┴────┴────╯
+           30   31   32 33   34   35
 */
 #include "helper.h"
 
-ZMK_COMBO(_BKSP, &kp BKSP, 20 21, 0)
-ZMK_COMBO(_DEL, &kp DEL, 8 9, 0)
-ZMK_COMBO(_ENTER, &kp ENTER, 19 20, 0)
-ZMK_COMBO(_ESC, &kp ESC, 14 15, 0)
-ZMK_COMBO(_INS, &kp INS, 7 8, 0)
-ZMK_COMBO(_TAB, &kp TAB, 15 16, 0)
+ZMK_COMBO(_BKSP,  &kp BKSP,   17 18, 0)
+ZMK_COMBO(_DEL,   &kp DEL,     7  8, 0)
+ZMK_COMBO(_ENTER, &kp ENTER,  16 27, 0)
+ZMK_COMBO(_ESC,   &kp ESC,    11 12, 0)
+ZMK_COMBO(_INS,   &kp INS,     6  7, 0)
+ZMK_COMBO(_TAB,   &kp TAB,    12 13, 0)
